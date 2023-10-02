df0a9195a6dac0023c2dd32f5ed15f8f84dd8048
Rodney Junior <rodney.junior37@gmail.com>
2023-10-01 22:02:09 -0400

    first commit

---
+++ src/main/java/com/chat/realtima/RealtimaApplication.java
@@ -0,0 +1,13 @@
+package com.chat.realtima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/realtima/RealtimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.realtima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
